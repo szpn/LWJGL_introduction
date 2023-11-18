4772d651789ccd514ff2cc16eb862b91649d2471
szpn <szopen1290@gmail.com>
2023-11-18 02:12:34 +0100

    fixed textures on demo textured cube

--- src/main/java/org/boot/DemoWorldObjects.java
+++ src/main/java/org/boot/DemoWorldObjects.java
@@ -20,14 +20,24 @@ public class DemoWorldObjects {
                 -0.5f,  -0.5f,   0.5f,
                 0.5f,   0.5f,   0.5f,
                 0.5f,   -0.5f,  0.5f,
+                //repeated vertices for top of the cube to fix the problem with texture mapping
+                -0.5f,  0.5f,   -0.5f,
+                0.5f,   0.5f,   -0.5f,
+                -0.5f,  0.5f,  0.5f,
+                0.5f,   0.5f,   0.5f,
+                //same for bottom of the cube
+                -0.5f,  -0.5f,  -0.5f,
+                0.5f,   -0.5f,  -0.5f,
+                -0.5f,  -0.5f,   0.5f,
+                0.5f,   -0.5f,  0.5f,
         };
         int[] indices = {
                 4,5,6,6,5,7, //front
                 0,1,2,2,1,3, //back
                 0,1,4,4,1,5, //left
                 6,7,2,2,7,3, // right
-                3,1,7,7,1,5, //bottom
-                2,0,6,6,0,4 //top
+                9,8,10,9,10,11, //top
+                13,12,14,13,14,15//bottom
         };
         float[] UVs = {
                 0f, 0f,
@@ -37,7 +47,15 @@ public class DemoWorldObjects {
                 1f, 0f,
                 1f, 1f,
                 0f, 0f,
-                0f, 1f
+                0f, 1f,
+                0f, 0f,
+                1f, 0f,
+                0f, 1f,
+                1f, 1f,
+                0f, 0f,
+                1f, 0f,
+                0f, 1f,
+                1f, 1f
         };
         Mesh mesh = MeshLoader.createTexturedMesh(vertices, indices, UVs);
         WorldObject demoTexturedObject = new WorldObject(mesh);
